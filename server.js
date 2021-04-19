cb288793c3b4b550b490b8bed1fe614aaadf9d2e
Louafi Noureddine <noureddinelouafi8@gmail.com>
2021-04-19 15:57:37 +0000

    Enablng the cross origin

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import schema from "./schema.js";
+import cors from "cors";
 
+//Initializing an Express server
 const app = express();
+
+//Allowing cross origin
+app.use(cors());
 
 app.use(
   "/graphql",
